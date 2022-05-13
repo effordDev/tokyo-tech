7e30b8f6817d2befc25e6b3d53c3febcfcb04ccf
Reid Efford <reidefford@gmail.com>
2022-05-13 12:53:01 -0400

    flip image profile

--- src/modules/ui/profileAvi/profileAvi.js
+++ src/modules/ui/profileAvi/profileAvi.js
@@ -3,10 +3,15 @@ import { api, LightningElement } from 'lwc';
 export default class ProfileAvi extends LightningElement {
      @api image = ''
      @api backupImage = ''
+     @api allowFlip = false
      @api size = 'large'
 
      get style() {
-          return `img ${this.size}`
+          return `img ${this.size} ${this.flip ? 'flipper' : ''}`
+     }
+
+     get flip() {
+          return this.allowFlip == 'true'
      }
      // get hasBackup() {
      //      return !!this.backupImage.length
