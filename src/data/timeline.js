440cab8595cf780627b8ea8a379310de028ef9b3
Reid Efford <reidefford@gmail.com>
2022-05-26 18:31:59 -0400

    timeline start

---
+++ src/data/timeline.js
@@ -0,0 +1,22 @@
+const fetchTimelineDate = () => {
+     return [
+          {
+               id: 0,
+               month: 4,
+               year: 2022,
+               caption: 'My last day at the Flagler humane society',
+               imgPath: '/public/assets/images/timeline/cage.jpg',
+          },
+          {
+               id: 1,
+               month: 4,
+               year: 2022,
+               caption: 'My first day home!',
+               imgPath: '/public/assets/images/timeline/home.jpg',
+          },
+     ]
+}
+
+export {
+     fetchTimelineDate
+}
